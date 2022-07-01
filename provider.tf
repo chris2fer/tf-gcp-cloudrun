33632fae7c4911d1010f122adc82f24e48b8c4d9
Christopher Dearie <cdearie@gmail.com>
2022-07-01 20:26:05 +0000

    backendstate

--- provider.tf
+++ provider.tf
@@ -2,3 +2,10 @@ provider "google" {
   project     = var.project_id
   region      = "us-central1"
 }
+
+terraform {
+  backend "gcs" {
+    bucket  = "cdearie-tf-states"
+    prefix  = "tf-gcp-cloudrun"
+  }
+}
